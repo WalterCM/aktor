3467469c309f306a6072e9a1fa7f05f7c158e721
Walter Capa <walterc316@gmail.com>
2016-05-09 00:48:22 -0500

    Added QTableView and QSqlQueryModel based classes.

--- src/models.py
+++ src/models.py
@@ -2,18 +2,63 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 from PyQt5.QtCore import Qt
-from PyQt5.QtSql import QSqlDatabase, QSqlQueryModel
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel
 
 class EquipmentModel(QSqlQueryModel):
     def __init__(self):
         super().__init__()
-        db = QSqlDatabase.addDatabase("QSQLITE")
-        db.setDatabaseName("../data/aktor.db")
-        if db.open():
-            print("opened")
-        else:
-            print("not opened")
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        dir = os.path.dirname(os.path.realpath('__file__'))
+        filename = os.path.join(dir, "data/aktor.db")
 
-        self.setQuery("SELECT * FROM Equipment")
-        self.setHeaderData(0, Qt.Horizontal, "Codigo")
+        self.db.setDatabaseName(filename)
+        if not self.db.open():
+            return -1
+
+        self.refresh()
+
+    def flags(self, index):
+        flags = QSqlQueryModel.flags(self, index)
+        if index.column() == 6:
+            flags |= Qt.ItemIsEditable
+        return flags
+
+    def setData(self, index, value, role):
+        if index.column() != 6:
+            return False
+
+        primaryKeyIndex = QSqlQueryModel.index(self, index.row(), 1)
+        id = self.data(primaryKeyIndex)
+
+        self.clear()
+        ok = self.setState(id, str(value))
+        self.refresh()
+        return ok
+
+    def setState(self, item_id, state):
+        query = QSqlQuery()
+        query.prepare("UPDATE Items SET state = ? WHERE id = ?")
+        query.addBindValue(state)
+        query.addBindValue(item_id)
+        return query.exec_()
+
+    def refresh(self):
+        self.setQuery("SELECT  d.description,\
+                               i.id,\
+                               i.serie_direjandro,\
+                               d.brand,\
+                               d.model,\
+                               i.serie,\
+                               i.state\
+                       FROM Items as i\
+                       INNER JOIN Devices as d\
+                       ON i.device_id = d.id")
+        self.setHeaderData(0, Qt.Horizontal, "Descripcion")
+        self.setHeaderData(1, Qt.Horizontal, "Item\ndirejandro")
+        self.setHeaderData(2, Qt.Horizontal, "Serie\nDirejandro")
+        self.setHeaderData(3, Qt.Horizontal, "Marca")
+        self.setHeaderData(4, Qt.Horizontal, "Modelo")
+        self.setHeaderData(5, Qt.Horizontal, "Serie")
+        self.setHeaderData(6, Qt.Horizontal, "Estado")
--- src/views.py
+++ src/views.py
@@ -2,8 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QTableView, QApplication
 from models import EquipmentModel
+from PyQt5.QtCore import Qt, QVariant
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QItemDelegate
+from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QTableView
+from PyQt5.QtWidgets import QAbstractItemView
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QPushButton
+
+
+class ComboBoxDelegate(QItemDelegate):
+    def __init__(self, parent, items):
+        super().__init__(parent)
+        self.items = items
+
+    def createEditor(self, parent, option, index):
+        editor = QComboBox(parent)
+        editor.addItems(self.items)
+
+        return editor
+
+    def setEditorData(self, editor, index):
+        value = str(index.model().data(index, Qt.EditRole))
+
+        num = self.items.index(value)
+        editor.setCurrentIndex(num)
+
+
+    def setModelData(self, editor, model, index):
+        value = editor.currentText()
+        print(value)
+        model.setData(index, value, Qt.DisplayRole)
+
+    def upadteEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
 
 class EquipmentView(QTableView):
 
@@ -14,13 +50,42 @@ class EquipmentView(QTableView):
     def initUI(self):
         equipment = EquipmentModel()
         self.setModel(equipment)
+        items = []
+        items.append("Disponible")
+        items.append("Prestado")
+        items.append("Perdido")
+        delegate = ComboBoxDelegate(self, items)
+        self.setItemDelegateForColumn(6, delegate)
+        self.setSelectionMode(QAbstractItemView.MultiSelection)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
 
-        self.setGeometry(300, 300, 290, 150)
-        self.setWindowTitle('sql test')
-        self.show()
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
         
+    def initUI(self):
+        self.mainWidget = QWidget()
+        self.setCentralWidget(self.mainWidget)
+
+        vbox = QVBoxLayout()
+        self.mainWidget.setLayout(vbox)
+
+        view = EquipmentView()
+        vbox.addWidget(view)
+
+        self.resize(view.rect().width(),
+                    view.rect().height())
+
+        button = QPushButton()
+        vbox.addWidget(button)
+        
+        self.setWindowTitle('aktor')
+        self.show()
+
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-    view = EquipmentView()
+    window = MainWindow()
     sys.exit(app.exec_())
